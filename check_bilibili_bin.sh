783e5fceebe02347433856d447eb6304f8cb9dfb
是一刀斩哒 <yidaozhanyaqwq@gmail.com>
2022-05-13 22:37:10 +0800

    Update check_bilibili_bin.sh

--- check_bilibili_bin.sh
+++ check_bilibili_bin.sh
@@ -10,21 +10,22 @@ fi
 echo "Current version is ${pkgver}"
 echo "Current SHA-256 checksum is ${sha256sums[0]}"
 echo "Checking for updates"
-
 VERSION=`curl -s 'https://api.bilibili.com/x/elec-frontend/update/latest.yml' | grep 'version:' | head -1 | sed '$s/.$//' | sed 's/version: //'`
+
+echo "Downloading Bilibili desktop ${VERSION}"
+wget -q "https://dl.hdslb.com/mobile/fixed/bili_win/bili_win-install.exe"
 BILI_HASH=`sha256sum bili_win-install.exe | awk -F" " '{print $1}'`
 echo "Latest version is ${VERSION}"
+echo "Latest SHA-256 checksum is ${BILI_HASH}"
 
-if [[ "${VERSION}" = "${pkgver}" ]];then
-  if [[ "${sha256sums[0]}" = "${BILI_HASH}" ]];then
+if [[ "${VERSION}" == "${pkgver}" ]];then
+  if [[ "${sha256sums[0]}" == "${BILI_HASH}" ]];then
   echo "AUR's is latest version, exiting"
   exit
   fi
 fi
 echo "Updating PKGBUILD to ${VERSION} $BILI_HASH"
 
-echo "Downloading Bilibili desktop ${VERSION}"
-wget -q "https://dl.hdslb.com/mobile/fixed/bili_win/bili_win-install.exe"
 
 echo "Replacing \$pkgver"
 sed -i "s/pkgver=${pkgver}/pkgver=${VERSION}/" ./bilibili-bin/PKGBUILD
